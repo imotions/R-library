60698138571a861521000783d98fb1859081dce2
Amandine <amandine.grappe@imotions.com>
2021-11-11 14:45:12 +0100

    Try removing build sh not used

--- imotionsApi/build.sh
+++
@@ -1,5 +0,0 @@
-Rscript R/installBuildDependencies.R
-Rscript -e devtools::document()
-cd target
-R CMD build --no-manual --no-build-vignettes ..
-R CMD check --no-manual --no-vignettes --no-examples *.tar.gz
